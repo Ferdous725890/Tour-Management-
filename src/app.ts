ba335624ce3e5959b42fa2c44fc4eb93421b1ee2
FERDOUS FERDOUS <you@example.com>
2025-08-08 12:18:57 +0600

    project-setup-completed

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from "express";
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Wellcome To Tour Management System Backend",
+  });
+});
+
+export default app;
+
+
+
+
+
+
+
+
+
+
+
+
+
