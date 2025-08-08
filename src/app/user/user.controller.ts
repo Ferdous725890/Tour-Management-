00f20eac8c4ab3f8ff49f53aecbec050372b848b
FERDOUS FERDOUS <you@example.com>
2025-08-09 00:11:17 +0600

    User Controller and Make Service Layer

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -2,15 +2,18 @@
 import { Request, Response } from "express";
 import { User } from "./user.model";
 import httpStatus from "http-status-codes"
+import { UserServices } from "./user.service";
 const CreatedUser = async (req: Request, res: Response) => {
   try {
     //recived paramitar
-    const { name, email } = req.body;
-    // created user
-    const user = await User.create({
-      name,
-      email,
-    });
+    // const { name, email } = req.body;
+    // // created user
+    // const user = await User.create({
+    //   name,
+    //   email,
+    // });
+
+const user = await UserServices.CreateUser(req.body)
 
     //send status
     res.status(httpStatus.CREATED).json({
